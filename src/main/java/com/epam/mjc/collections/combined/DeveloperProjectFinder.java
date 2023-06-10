28d0a87574f61dfc7c92e4bf21e40548bcfd8118
Violetta Prikhno <Violetta_Prikhno@epam.com>
2023-06-10 14:09:57 +0300

    My first commit!

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,28 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> result = new ArrayList<>();
+
+        for (Map.Entry<String, Set<String>> entry : projects.entrySet()) {
+            String projectName = entry.getKey();
+            Set<String> developers = entry.getValue();
+
+            if (developers.contains(developer)) {
+                result.add(projectName);
+            }
+        }
+
+        Collections.sort(result, (p1, p2) -> {
+            if (p1.length() != p2.length()) {
+                return Integer.compare(p2.length(), p1.length());
+            } else {
+                return p2.compareTo(p1);
+            }
+        });
+
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,28 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        HashMap<Integer, Set<String>>  result = new HashMap();
+
+        sourceMap.forEach((key, value) -> {
+            HashSet<String> newValue = new HashSet();
+            int keyLenght = key.length();
+
+
+            sourceMap.forEach((innerKey, innerValue) -> {
+                if(innerKey.length() == keyLenght){
+                    newValue.add(innerKey);
+                }
+            });
+
+            result.put(keyLenght, newValue);
+        });
+
+        return result;
     }
 }
